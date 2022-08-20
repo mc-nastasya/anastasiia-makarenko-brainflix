518f1928b7dc13b3b000c1cce32a4aa13f43b6f9
Anastasiia Makarenko <nacty02311@gmail.com>
2022-08-19 22:47:08 -0400

    add styles to AddedComments

--- src/components/AddedComments/AddedComments.js
+++ src/components/AddedComments/AddedComments.js
@@ -3,19 +3,27 @@ import "./AddedComments.scss"
 function AddedComments({selectedVideo}) {
     const comments = selectedVideo.comments;
 
+    function formatDate(date) {
+        const formattedDate = new Date(date);
+        const year = formattedDate.getFullYear();
+        const month = ( "0" + (formattedDate.getMonth() + 1)).slice(-2);
+        const day = ( "0" + formattedDate.getDate()).slice(-2);
+        return `${month}/${day}/${year}`;
+    }
+
     return (
     <div className="comments__added">
         {
             comments.map((comment)=>{
                 return(
-                    <div className="comments__card card" key={comment.id}>
+                    <div className="comments__card card" key={comment.timestamp}>
                         <div className="card__left">
                             <div className="card__image"></div>
                         </div>
                         <div className="card__right">
                             <div className="card__top">
                                 <p className="card__name">{comment.name}</p>
-                                <p className="card__date">{comment.timestamp}</p>
+                                <p className="card__date">{formatDate(comment.timestamp)}</p>
                             </div>
                             <p className="card__comment">{comment.comment}</p>
                         </div>
