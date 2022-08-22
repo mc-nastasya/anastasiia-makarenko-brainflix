a7a88efd3cb396a1e2e9e7197ea4aa1c40cf07db
Anastasiia Makarenko <nacty02311@gmail.com>
2022-08-22 17:02:05 -0400

    add assets

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Route path="/" element={<MainInfoContainer />}/>
         <Route path="/upload" element ={<PublishVideo />}/>
       </Routes>
-      
     </BrowserRouter>
   );
 }
--- src/components/PublishVideo/PublishVideo.js
+++ src/components/PublishVideo/PublishVideo.js
@@ -1,8 +1,24 @@
 import "./PublishVideo.scss";
+import VideoThumbnail from "../../assets/Images/Upload-video-preview";
 
 function PublishVideo() {
   return (
-    <div>PublishVideo</div>
+    <>
+        <h1>Upload Video</h1>
+        <div>
+            <p>Video thumbnail</p>
+            <img src={VideoThumbnail} alt=""/>
+        </div>
+        <form>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+
+        </form>
+    </>
   )
 }
 export default PublishVideo;
