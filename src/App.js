203075eec110de71bd63550a35d893522bfeb736
Anastasiia Makarenko <nacty02311@gmail.com>
2022-08-22 16:33:13 -0400

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import MainInfoContainer from './components/MainInfoContainer/MainInfoContainer';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import PublishVideo from './components/PublishVideo/PublishVideo';
 
 
 function App() {
   
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <MainInfoContainer />
-    </>
+      <Routes>
+        <Route path="/" element={<MainInfoContainer />}/>
+        <Route path="/upload" element ={<PublishVideo />}/>
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
