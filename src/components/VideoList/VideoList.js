a17224b279b5a21d15d996625010a0a2fd95f60b
Anastasiia Makarenko <nacty02311@gmail.com>
2022-09-04 19:11:42 -0400

    add ellipsis for video list

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,7 +1,36 @@
 import { Link } from "react-router-dom";
 import "./VideoList.scss";
+import {useState, useEffect} from 'react';
 
 function VideoList({nonSelectedVideos}) {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
+
+    const cutTitle = (string) =>{
+        if(string.length < 41) {
+            return string;
+        }
+        let result = "";
+        const newString = string.slice(0, 40);
+        if (string[39] === " " || string[40] === " ") {
+            result = newString;
+        } else {
+        result = newString.slice(0, newString.lastIndexOf(" "));
+        }
+        return result + "...";
+    }
+
+
     return (
         <div className="video-list">
             <p className="video-list__title">Next videos</p>
@@ -11,7 +40,7 @@ function VideoList({nonSelectedVideos}) {
                     <div  className="video-list__item item">
                         <img src={video.image} alt={video.title} className="item__image" />
                         <div className="item__right">
-                            <p className="item__title">{video.title}</p>
+                            <p className="item__title">{width< 768 ? cutTitle(video.title): video.title}</p>
                             <p className="item__channel">{video.channel}</p>
                         </div>
                     </div>
@@ -23,4 +52,4 @@ function VideoList({nonSelectedVideos}) {
     )
 }
 
-export default VideoList
+export default VideoList;
